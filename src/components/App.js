becce7dbcfad7cf156becaceac4bb86271b69fcd
fatGubari <fatimajubary@gmail.com>
2023-03-19 09:08:46 +0300

    completed assignemtn

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [dog, setDog] = useState(null)
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+          setDog(data.message);
+        });
+    },[])
+
+    if (!dog) {
+        return <p>Loading..</p>
+    }
+  return (
+    <div>
+        <img src={dog} alt="A Random Dog" />
+    </div>
+  )
+}
+
+export default App
